146366c77d2c4827a3e2c28297e3a09f58df26c2
sdzx1993 <shangdizhixia1993@gmail>
2020-03-22 18:14:39 +0800

    nice

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for servant1
+
+## Unreleased changes
---
+++ src/Lib.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE DeriveAnyClass  #-}
+{-# LANGUAGE DeriveGeneric   #-}
+{-# LANGUAGE TemplateHaskell #-}
+module Lib
+    ( someFunc
+    ) where
+
+import           Control.Lens
+import           Data.Aeson
+import           Data.Aeson
+import           Data.Char
+import           GHC.Generics
+import           Text.Pretty.Simple
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
+
+
+
+data P =
+  P { _name :: String
+    , _age  :: Int }
+  deriving (Eq,Show,Ord,Generic,FromJSON,ToJSON)
+
+data M = M {
+    _aaa :: String
+  , _bbb :: P
+           }
+  deriving (Eq,Show,Ord,Generic,FromJSON,ToJSON)
+
+
+makeLenses ''P
+makeLenses ''M
+
+m = M "bbbb" (P "yangMiao" 33)
+
+t1 = m ^. bbb . name
